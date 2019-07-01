6341e7675bb7082c7582e094b76e729882ebe048
mcbarlowe <mcbarlowe@gmail.com>
2019-07-01 01:37:11 -0400

    Autocomplete finished for player names

--- app.py
+++ app.py
@@ -13,14 +13,22 @@ def index():
     default_season = '2019'
     default_player = 'LeBron James'
     if request.form:
-        print(request.form)
         player = request.form.get('player')
         season = request.form.get('season')
+        players = db.session.query(playerbygamestats.player_name, playerbygamestats.toc)\
+                .filter(playerbygamestats.toc > 0).distinct(playerbygamestats.player_name).all()
+        players = [row[0] for row in players]
+        print(players)
         data = playerbygamestats.query.filter((playerbygamestats.season == season) & (playerbygamestats.player_name == player))
-        return render_template('index.html', value=data, default_season=season, default_player=player)
+        return render_template('index.html', value=data, default_season=season, default_player=player, auto=players)
     else:
-        data = playerbygamestats.query.filter(playerbygamestats.game_date == '2016-10-25').all()
-        return render_template('index.html', value=data, default_season=default_season, default_player=default_player)
+        data = playerbygamestats.query\
+                .filter(playerbygamestats.game_date == '2016-10-25').all()
+        players = db.session.query(playerbygamestats.player_name, playerbygamestats.toc)\
+                .filter(playerbygamestats.toc > 0).distinct(playerbygamestats.player_name).all()
+        print(players)
+        players = [row[0] for row in players]
+        return render_template('index.html', value=data, default_season=default_season, default_player=default_player, auto=players)
 
 
 if __name__ == '__main__':
