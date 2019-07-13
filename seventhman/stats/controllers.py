93dabe88adb19d4db645a352f8faf70840fb14ff
mcbarlowe <mcbarlowe@gmail.com>
2019-07-13 00:03:30 -0400

    stats page now shows average of all stats need to look at how orebs are calculated in scraper

--- seventhman/stats/controllers.py
+++ seventhman/stats/controllers.py
@@ -25,8 +25,26 @@ def index():
                 with_entities(playerbygamestats.player_name,
                               playerbygamestats.season,
                               func.string_agg(playerbygamestats.team_abbrev.distinct(),
-                                              aggregate_order_by(literal_column("'/'"), playerbygamestats.team_abbrev.desc())).label('Teams'),
-                              func.avg(playerbygamestats.fgm))\
+                                              aggregate_order_by(literal_column("'/'"),
+                                                                 playerbygamestats.team_abbrev.desc())).label('teams'),
+                              func.count(playerbygamestats.player_id).label('gp'),
+                              func.round(func.avg(playerbygamestats.toc)/60, 1).label('mins'),
+                              func.round(func.avg(playerbygamestats.fgm), 1).label('fgm'),
+                              func.round(func.avg(playerbygamestats.fga), 1).label('fga'),
+                              func.round(func.avg(playerbygamestats.tpm), 1).label('tpm'),
+                              func.round(func.avg(playerbygamestats.tpa), 1).label('tpa'),
+                              func.round(func.avg(playerbygamestats.ftm), 1).label('ftm'),
+                              func.round(func.avg(playerbygamestats.fta), 1).label('fta'),
+                              func.round(func.avg(playerbygamestats.oreb), 1).label('oreb'),
+                              func.round(func.avg(playerbygamestats.dreb), 1).label('dreb'),
+                              func.round(func.avg(playerbygamestats.ast), 1).label('ast'),
+                              func.round(func.avg(playerbygamestats.tov), 1).label('tov'),
+                              func.round(func.avg(playerbygamestats.stl), 1).label('stl'),
+                              func.round(func.avg(playerbygamestats.blk), 1).label('blk'),
+                              func.round(func.avg(playerbygamestats.pf), 1).label('pf'),
+                              func.round(func.avg(playerbygamestats.points), 1).label('points'),
+                              func.round(func.avg(playerbygamestats.plus_minus), 1).label('plus_minus')
+                              )\
                         .group_by(playerbygamestats.player_name,
                                   playerbygamestats.player_id,
                                   playerbygamestats.season).\
